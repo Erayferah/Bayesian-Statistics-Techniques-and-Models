2abf28cfc66afe5cb585f48b4e6883364d1af8d9
GNZXH <eray.ferah@bayer.com>
2022-10-13 08:23:42 +0200

    synch up

---
+++ Week-3/7-part-a-r-script.r
@@ -0,0 +1,32 @@
+library("car")  # load the 'car' package
+data("Anscombe")  # load the data set
+?Anscombe  # read a description of the data
+head(Anscombe)  # look at the first few lines of the data
+pairs(Anscombe)  # scatter plots for each pair of variables
+
+model_non_inf <- lm("education ~ income + young + urban", data=Anscombe)
+summary(model_non_inf)
+
+library("rjags")
+
+mod_string = " model {
+    for (i in 1:length(education)) {
+        education[i] ~ dnorm(mu[i], prec)
+        mu[i] = b0 + b[1]*income[i] + b[2]*young[i] + b[3]*urban[i]
+    }
+    
+    b0 ~ dnorm(0.0, 1.0/1.0e6)
+    for (i in 1:3) {
+        b[i] ~ dnorm(0.0, 1.0/1.0e6)
+    }
+    
+    prec ~ dgamma(1.0/2.0, 1.0*1500.0/2.0)
+    	## Initial guess of variance based on overall
+    	## variance of education variable. Uses low prior
+    	## effective sample size. Technically, this is not
+    	## a true 'prior', but it is not very informative.
+    sig2 = 1.0 / prec
+    sig = sqrt(sig2)
+} "
+
+data_jags = as.list(Anscombe)
